0bd5b0a063cfbf16b51eac4dfbf2375288b7fc7e
yhellow <61879663+yhellow@users.noreply.github.com>
2020-06-20 03:03:35 +0900

    add section04-1

--- lecture/section03-1.py
+++ lecture/section03-1.py
@@ -47,10 +47,11 @@
 
 
 # Testing installed package
-
 import simplejson as json
 
 test_dict = {'1': 95, '4': 77, '3': 65, '5': 100, '2': 88}
 
 #simplejson 실행
 print(json.dumps(test_dict, sort_keys=True, indent=4 * ' '))
+
+
--- lecture/section04-1.py
+++ lecture/section04-1.py
@@ -1,15 +1,18 @@
 # Section04
-# data types
+# data types and math operations
 
 """
     1. boolean
     2. number
-    3. string
+    3. string (sequence)
     4. bytes
-    5. list
-    6. tuple
+    5. list (sequence)
+    6. tuple (sequence)
     7. set
     8. dictionary
+        bytearray
+        byte
+        frozenset
 """
 
 # variable_datatype
@@ -39,6 +42,7 @@ print()
 
 
 #  operations e.g.
+#  python math: https://docs.python.org/3/library/stdtypes.html#numeric-types-int-float-complex
 i1 = 29
 i2 = 929
 bigi1 = 3333333333
@@ -50,6 +54,11 @@ f4 = 10.
     # multiply
 print(i1 * i2)
 print(bigi1 * bigi2)
+    # division
+    # / v // v %
+print(10/3)
+print(10//3)
+print(10%3)
     # squared
 print(f1 ** f2)
     # integer + float = float
@@ -76,4 +85,74 @@ isfloat(f3i2)
 print(isfloat(f3i2))
 print()
 
+
+# changing data types
+# int(), float(), complex()
+a = 1.
+b = 2
+c = 3
+print(type(a), type(b))
+fint = a + b
+print(fint)
+print(type(fint))
+    # from '9.0' to '9' v '9' to '9.0'
+print(int(fint))
+print(float(c))
+    # int to complex
+print(complex(b))
+    # boolean as integers
+    # False == 0 / True == 1
+    # !! has to have capital first letter
+print(int(False))
+print(int(True))
+    # string to integer
+print(int('4'))
+    # '0' as complex
+print(complex(False))
+print()
+
+
+# repeatable operations
+"""
+    y = 100
+    y = y + 100
+    is equal to:
+    y += 100
+"""
+    # !! operation comes before '=' 
+y = 100
+y = y + 100
+print(y)
+x = 100
+x += 100
+print(x)
+print()
+
+
+# complex operations
+"""
+    abs(x): absolute value or magnitude of x
+    complex(re, im): a complex number with real part re, imaginary part im. im defaults to zero
+    .conjugate(): conjugate of the complex number c
+    divmod(x, y): the pair (x // y, x % y)
+    pow(x, y): x to the power y
+"""
+
+# divmod()
+dm = divmod(100,9)
+    # as combination
+print(dm)
+    # as separate numbers
+n, m = dm
+print(n, m)
+print()
+
+
+# math module
+import math
+    # math.ceil(x): integer that is smallest but bigger than x
+print(math.ceil(5.1))
+    # math.floor(x): integer that is largest but smaller than x
+print(math.floor(5.1))
+print()
 
